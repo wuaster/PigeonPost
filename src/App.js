facb81f1a4544e7d836d1224c545a668d2915300
wuaster <>
2019-12-17 01:06:44 -0500

    Adding text field entry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react"
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import ReactDOM from 'react-dom'
+import 'typeface-roboto';
 import logo from "./logo.svg"
 import "./App.css"
 
 class LambdaDemo extends Component {
   constructor(props) {
     super(props)
-    this.state = { loading: false, msg: null }
+    this.state = { loading: false, msg: "Send it into the void or send an anonymous message." }
   }
 
   handleClick = api => e => {
     e.preventDefault()
-
+    ReactDOM.render(<App/>, document.getElementById('root'));
     this.setState({ loading: true })
     fetch("/.netlify/functions/" + api)
       .then(response => response.json())
@@ -19,13 +24,31 @@ class LambdaDemo extends Component {
 
   render() {
     const { loading, msg } = this.state
-
+    const buttonStyle = {
+      margin: "12px",
+    };
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
+        
         <span>{msg}</span>
+        <br />
+        <Button
+        variant="contained"
+        color="secondary"
+        onClick={this.handleClick("hello")}
+        style={buttonStyle}
+      >
+        {loading ? "Sending..." : "Send to the Void"}
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        style={buttonStyle}
+      >
+        {loading ? "Sending..." : "Send to the Skies"}
+      </Button>
+
+        
       </p>
     )
   }
@@ -33,13 +56,21 @@ class LambdaDemo extends Component {
 
 class App extends Component {
   render() {
+    const textAreaStyle = {
+      width: "60%",
+      color: "#515b61",
+      backgroundColor: "#bfe0f7",
+      padding: "10px",
+      fontFamily: "Arial",
+      fontSize: "calc(10px + 1vmin)",
+      borderRadius: "6px",
+    };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>All messages are 100% anonymous. Play nice.</p>
+          <TextareaAutosize aria-label="empty textarea" id="textinput" rowsMin={25} rowsMax={25} placeholder="" style={textAreaStyle} />
           <LambdaDemo />
         </header>
       </div>
